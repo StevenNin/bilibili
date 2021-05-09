cc05811d19d1c9cab6311f4d86494e943bc49c02
steven <zou995201314@163.com>
2021-05-09 17:30:48 +0800

    新增

---
+++ src/components/common/loginTop.vue
@@ -0,0 +1,42 @@
+<template>
+  
+  <div class="navTop">
+      <div></div>
+      <div>{{topTitle}}</div>
+      <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"loginTop",
+    props:{
+        topTitle:{
+            type:String,
+        },
+    },
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<style lang="less" scoped>
+@import "../../assets/base.less";
+    .navTop{
+        height: 50px;
+        // border: 1px solid red;
+        display: flex;
+        background-color: @themeColor;
+        div{
+            color: @backcolor;
+            display: flex;
+            flex: 1;
+            justify-content: center;
+            align-items: center;    
+            font-size: 4vw; 
+        }
+    }
+</style>
---
+++ src/views/register.vue
@@ -0,0 +1,16 @@
+<template>
+  <loginTop :topTitle="'注册账号'"></loginTop>
+</template>
+
+<script>
+import loginTop from "@/components/common/loginTop"
+
+export default {
+    name:"register",
+    components:{loginTop},
+}
+</script>
+
+<style>
+
+</style>
